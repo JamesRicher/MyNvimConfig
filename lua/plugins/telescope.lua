3902e2c5866f442249c7f2d1eef12b8df74dcd7c
James Richer <richerjames@gmail.com>
2025-10-24 14:58:41 +0100

    Added case sesnitivity for live grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,14 @@ return
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         local builtin = require("telescope.builtin")
+        local telescope = require("telescope")
+        telescope.setup {
+            defaults = {
+                vimgrep_arguments = {
+                    '--case-sensitive',
+                },
+            },
+        }
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
